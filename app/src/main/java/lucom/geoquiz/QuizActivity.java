d429250916847b7c4651ddbfea10b21881491e0c
Carlos <astaroth86@gmail.com>
2016-01-27 00:50:53 +0100

    Update QuizActivity.java

--- app/src/main/java/lucom/geoquiz/QuizActivity.java
+++ app/src/main/java/lucom/geoquiz/QuizActivity.java
@@ -1,52 +1,103 @@
 package lucom.geoquiz;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
+
+    private Button mBotonVerdadero;
+    private Button mBotonFalso;
+    private ImageButton mBotonSiguiente;
+    private ImageButton mBotonAnterior;
+    private TextView mTextoPregunta;
+
+    private Pregunta mpreguntas[] = new Pregunta[]{
+            new Pregunta(R.string.pregunta_Africa, false),
+            new Pregunta(R.string.pregunta_America, false),
+            new Pregunta(R.string.pregunta_Asia, false),
+            new Pregunta(R.string.pregunta_europa, true),
+            new Pregunta(R.string.pregunta_oceania, true),
+            new Pregunta(R.string.pregunta_oceanos, false)
+    };
+
+    private int mPosicionPregunta = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        mTextoPregunta = (TextView) findViewById(R.id.txt_Quiz_pregunta);
+        mTextoPregunta.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                mPosicionPregunta = (mPosicionPregunta + 1) % mpreguntas.length;
+                actualizarPregunta();
+            }
+        });
+        actualizarPregunta();
+
+        mBotonVerdadero = (Button) findViewById(R.id.boton_verdadero);
+        mBotonVerdadero.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                //Aquí su acción
+                comprobarPregunta(true);
+            }
+        });
+
+        mBotonFalso = (Button) findViewById(R.id.boton_falso);
+        mBotonFalso.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Aquí su acción
+                comprobarPregunta(false);
+            }
+        });
+
+        mBotonSiguiente = (ImageButton) findViewById(R.id.boton_siguiente);
+        mBotonSiguiente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Aquí su acción
+                mPosicionPregunta = (mPosicionPregunta + 1) % mpreguntas.length;
+                actualizarPregunta();
+
+            }
+        });
+
+        mBotonAnterior = (ImageButton) findViewById(R.id.boton_anterior);
+        mBotonAnterior.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Aquí su acción
+                if(mPosicionPregunta != 0){
+                    mPosicionPregunta = (mPosicionPregunta - 1) % mpreguntas.length;
+                    actualizarPregunta();
+                }
             }
         });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_quiz, menu);
-        return true;
+    private void actualizarPregunta(){
+        int pregunta = mpreguntas[mPosicionPregunta].getTextResId();
+        mTextoPregunta.setText(pregunta);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    private void comprobarPregunta(boolean presionadoPorUsuario){
+        boolean respuestaVerdadera = mpreguntas[mPosicionPregunta].isRespuestaVerdadera();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        int mensajeResId = 0;
+
+        if(presionadoPorUsuario == respuestaVerdadera){
+            mensajeResId = R.string.toast_correcto;
+        } else {
+            mensajeResId = R.string.toast_incorrecto;
         }
-
-        return super.onOptionsItemSelected(item);
+        Toast.makeText(QuizActivity.this, mensajeResId, Toast.LENGTH_SHORT).show();
     }
 }
