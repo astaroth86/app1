c9d3557d2ef8f52e1b040015cfde2f5da89e4cfa
astaroth <astaroth86@gmail.com>
2016-06-12 20:39:55 +0200

    uytu

--- app/src/main/java/lucom/geoquiz/QuizActivity.java
+++ app/src/main/java/lucom/geoquiz/QuizActivity.java
@@ -2,6 +2,7 @@ package lucom.geoquiz;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -9,6 +10,8 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
+
+    private static final String TAG = "QuizActivity";
 
     private Button mBotonVerdadero;
     private Button mBotonFalso;
@@ -25,11 +28,12 @@ public class QuizActivity extends AppCompatActivity {
             new Pregunta(R.string.pregunta_oceanos, false)
     };
 
-    private int mPosicionPregunta = 0;
+    private int mPosicionPregunta = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate(Bundle) called");
         setContentView(R.layout.activity_quiz);
 
         mTextoPregunta = (TextView) findViewById(R.id.txt_Quiz_pregunta);
@@ -99,5 +103,35 @@ public class QuizActivity extends AppCompatActivity {
             mensajeResId = R.string.toast_incorrecto;
         }
         Toast.makeText(QuizActivity.this, mensajeResId, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
     }
 }
